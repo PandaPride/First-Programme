70c9671b6b0af13ade0727ad736d40649fa64a97
PandaPride <167537757+PandaPride@users.noreply.github.com>
2024-05-06 19:37:09 +0700

    Changing Words
    World -> Mars
    Add Birth Date

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-    echo "Hello World <br>";
+    echo "Hello Mars <br>";
     echo "<---- BIODATA DIRI ----> <br>";
     echo "Nama :  Richelle C. W. <br>";
     echo "Umur :  16 Tahun <br>";
     echo "Tempat Lahir : Jakarta <br>";
+    echo "Tanggal Lahir : 26 April 2008 <br>";
     echo "Pekerjaan : Siswa SMA <br>";
     echo "Kewarganegaraan : WNI ";
     
