b0fabf60d547fea4237d9d958b5d49482d07de16
PandaPride <167537757+PandaPride@users.noreply.github.com>
2024-05-06 19:34:17 +0700

    Nothing to see just uploading....

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Hello World <br>";
+    echo "<---- BIODATA DIRI ----> <br>";
+    echo "Nama :  Richelle C. W. <br>";
+    echo "Umur :  16 Tahun <br>";
+    echo "Tempat Lahir : Jakarta <br>";
+    echo "Pekerjaan : Siswa SMA <br>";
+    echo "Kewarganegaraan : WNI ";
+    
+
+?>
